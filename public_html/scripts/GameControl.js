0cbc9e3048e6a1287f0faeec1c3bf6e1df1e8688
Austin Macdonald <austin@redhat.com>
2019-10-19 13:03:50 -0400

    basic movement

--- public_html/scripts/GameControl.js
+++ public_html/scripts/GameControl.js
@@ -1,26 +1,36 @@
 /* global engine, junkRenderer */
+function Initialize(){
+    var name = "ISS";
+    var x = junkRenderer.getWidth() / 4;
+    var y = 10;
+    var r = 10;
+    var hue = 100;
+    var lit = 50;
+    var sat = 50;
+    spaceObjects.push(new SpaceObject(name, "", x, y, r, hue, sat, lit, 1));
+}
 
 function GameControl(){
     var interval;
     var intervalLength = 30;
-    
+
     this.play = function(){
         interval = window.setInterval(gameLoop,intervalLength);
     };
-    
+
     this.pause = function(){
         window.clearInterval(interval);
     };
-    
-    
+
+
     function gameLoop(){
         //deal with any user input
-        
-        
+
+
         //update Physics Engine
         engine.increment(1);
-        
-        
+
+
         //ask renderer to draw
         junkRenderer.update();
     }
--- public_html/scripts/JunkPhysics.js
+++ public_html/scripts/JunkPhysics.js
@@ -2,24 +2,24 @@
 /* global ship */ //the ship, it is from where pellets are fired
 
 function JunkPhysics(){
-    
-    
+
+
     this.increment = function(t){
         //increment through all space objects and update their x and y
         for(var i = 0; i < spaceObjects.length; i++){
-            
+            spaceObjects[i].x = (spaceObjects[i].x+5) % junkRenderer.getWidth()
         }
-        
+
         //do the same for the ship
-        
-        
+
+
     };
-    
+
     this.firePellet = function(theta){
-        
-        
-        
-        
+
+
+
+
     };
-    
+
 }
